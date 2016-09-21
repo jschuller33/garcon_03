9be1210f70d0cd153a9cbb333e7f2369253affe8
John Schuller <Renegade1@Johns-MacBook-Pro.local>
2016-09-21 16:51:38 -0500

    trying different greetings

--- app.py
+++ app.py
@@ -52,6 +52,17 @@ def webhook():
 
     return "ok", 200
 
+# Sentences we'll respond with if the user greeted us
+GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup", "what's up",)
+
+GREETING_RESPONSES = ["'sup bro", "hey", "*nods*", "hey you get my snap?"]
+
+def check_for_greeting(sentence):
+    """If any of the words in the user's input was a greeting, return a greeting response"""
+    for word in sentence.words:
+        if word.lower() in GREETING_KEYWORDS:
+            return random.choice(GREETING_RESPONSES)
+
 
 def send_message(recipient_id, message_text):
 
