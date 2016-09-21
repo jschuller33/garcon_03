8573dc2e7eb0ec248151c74cdbcbac1e8723a262
John Schuller <Renegade1@Johns-MacBook-Pro.local>
2016-09-21 17:13:39 -0500

    hello test

--- app.py
+++ app.py
@@ -33,11 +33,14 @@ def webhook():
         for entry in data["entry"]:
             for messaging_event in entry["messaging"]:
 
-                if messaging_event.get("message") = "hello" :  # someone sent us a message
+                if messaging_event.get("message"):  # someone sent us a message
 
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
+                        if message_text = ("hello"):
+                            send_message(sender_id, "hello sir")
+                            pass
 
                     send_message(sender_id, "this is the message to be sent back")
 
